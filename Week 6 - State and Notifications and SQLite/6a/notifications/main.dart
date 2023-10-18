1b5dca3b5ff61232a18964a90f3102ab8b86bcd8
Michael Miljanovic <michael.miljanovic@uoit.ca>
2023-10-18 10:36:25 -0400

    6a notifications not working

---
+++ Week 6 - State and Notifications and SQLite/6a/notifications/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tzdata;
+
+void main() async {
+  tzdata.initializeTimeZones();
+  WidgetsFlutterBinding.ensureInitialized();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  final AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('app_icon');
+  // final IOSInitializationSettings initializationSettingsIOS =
+  // IOSInitializationSettings();
+  final InitializationSettings initializationSettings =
+  InitializationSettings(
+      android: initializationSettingsAndroid,
+//      iOS: initializationSettingsIOS
+      );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize notifications
+    final AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');
+    // final IOSInitializationSettings initializationSettingsIOS =
+    // IOSInitializationSettings();
+    final InitializationSettings initializationSettings =
+    InitializationSettings(
+        android: initializationSettingsAndroid,
+//        iOS: initializationSettingsIOS
+    );
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  // Function to show a Snackbar
+  void _showSnackbar(BuildContext context, String scheduledTime) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text('Notification scheduled for $scheduledTime'),
+    ));
+  }
+
+  // Function to schedule a notification
+  Future<void> _scheduleNotification(BuildContext context, DateTime scheduledTime) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      channelDescription: 'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    // const IOSNotificationDetails iOSPlatformChannelSpecifics =
+    // IOSNotificationDetails();
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+  //    iOS: iOSPlatformChannelSpecifics,
+    );
+
+    final timeZone = tz.local;
+
+    final scheduledNotificationDateTime =
+    tz.TZDateTime.from(scheduledTime, timeZone);
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Scheduled Notification',
+      'This is a scheduled notification.',
+      scheduledNotificationDateTime,
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+    );
+
+    _showSnackbar(context, 'Notification scheduled for $scheduledTime');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('Notification App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () async {
+                  // Show an immediate notification
+                  await flutterLocalNotificationsPlugin.show(
+                    0,
+                    'Immediate Notification',
+                    'This is an immediate notification.',
+                    NotificationDetails(
+                      android: AndroidNotificationDetails(
+                        'your_channel_id',
+                        'your_channel_name',
+                        channelDescription: 'your_channel_description',
+                        importance: Importance.max,
+                        priority: Priority.high,
+                      ),
+                      //iOS: IOSNotificationDetails(),
+                    ),
+                  );
+                  _showSnackbar(context, 'Immediate notification displayed');
+                },
+                child: Text('Show Immediate Notification'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // Show a date/time picker for scheduling
+                  final selectedTime = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2101),
+                  );
+                  if (selectedTime != null) {
+                    final selectedDateTime = DateTime(
+                      selectedTime.year,
+                      selectedTime.month,
+                      selectedTime.day,
+                      12,
+                      0, // Assuming notifications are scheduled for noon
+                    );
+                    await _scheduleNotification(context, selectedDateTime);
+                  }
+                },
+                child: Text('Schedule Notification'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
